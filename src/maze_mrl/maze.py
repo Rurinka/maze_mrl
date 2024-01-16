7239482de055663359577ac7b7796caebd798f28
rurinka <7807asd@gmail.com>
2024-01-16 16:52:14 +0900

    new commit

--- src/maze_mrl/maze.py
+++ src/maze_mrl/maze.py
@@ -1,4 +1,13 @@
-def main():
-    print("hi")
-main()
-print("hi")
+# import pygame
+#
+# pygame.init()
+# screen = pygame.display.set_mode((640, 480))
+# pygame.display.set_caption("Hello World")
+# while True:
+#    for event in pygame.event.get():
+#       if event.type == pygame.QUIT:
+#          pygame.quit()
+#
+size = 10
+maze = [["██" for j in range(size)] for i in range(size)]
+print('\n'.join([''.join(i) for i in maze]))
